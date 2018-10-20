fd69df2408d60ac5c4dd47e0e0a8e393d65707ce
kyleworley <kworley97@gmail.com>
2018-10-20 12:14:39 -0500

    inital base commit

---
+++ data.py
@@ -0,0 +1,30 @@
+def Articles():
+    articles = [
+    
+            {
+                'id': 1,
+                'title':'Article One',
+                'body':'Lorem ipsum asljasdl;fjasdf',
+                'author':'Kyle',
+                'create_data':'13 December 2017'
+            },
+            {
+                'id': 2,
+                'title':'Article Two',
+                'body':'Lorem ipsum asljasdl;fjasdf',
+                'author':'Kyle',
+                'create_data':'13 December 2017'
+            },
+            {
+                'id': 3,
+                'title':'Article Three',
+                'body':'Lorem ipsum asljasdl;fjasdf',
+                'author':'Kyle',
+                'create_data':'13 December 2017'
+            }
+    ]
+    return articles
+
+
+
+
